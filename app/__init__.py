85b2a33b1dbe4591a940bd4611e6c4e01c4a3bb4
vlad <vlad1256a@gmail.com>
2024-11-25 18:32:16 +0200

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('../config.py')
+
+    db.init_app(app)
+
+    # Реєстрація маршрутів
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
