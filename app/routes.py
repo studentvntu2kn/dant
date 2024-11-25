584bdf6d3986aad4f8ec90be28886d1e9fd2ffd8
studentvntu2kn <01-24-291.stud@vntu.vn.ua>
2024-11-25 19:38:27 +0200

    extend code base

--- app/routes.py
+++ app/routes.py
@@ -6,18 +6,31 @@ main = Blueprint('main', __name__)
 def home():
     return render_template('index.html')
 
+@main.route('/home')
+def home2():
+    return render_template('index.html')
+
 @main.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('info/about.html')
 
 @main.route('/review')
 def review():
-    return render_template('review.html')
+    return render_template('info/review.html')
 
 @main.route('/services')
 def services():
-    return render_template('services.html')
+    return render_template('info/services.html')
 
 @main.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('info/contact.html')
+
+
+@main.route('/register')
+def register():
+    return render_template('auth/register.html')
+
+@main.route('/login')
+def login():
+    return render_template('auth/login.html')
